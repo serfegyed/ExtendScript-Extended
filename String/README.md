58462d7922e7f20a0fd5fde5c5fda59ece17d484
serfegyed <serfegyed@users.noreply.github.com>
2024-01-20 21:38:04 +0100

    deleted: chop(), chopEnd(), chopStart() because of extended 'trim' group

--- String/README.md
+++ String/README.md
@@ -20,16 +20,13 @@ These are possible implementation of the JavaScript built-in String methods.
 - `repeat()`		:	Constructs and returns a new string which contains the specified number of copies of the string on which it was called
 - `replaceAll()`	:	Replaces all occurrences of a specified search string or regular expression
 - `startsWith()`	:	Determines whether a string begins with the characters of a specified string, returning true or false as appropriate
-- `trim()`			:	Removes the spaces from both ends of the string.
-- `trimEnd()`		:	Removes the spaces from the end of the string.
-- `trimStart()`		:	Removes whitespace from the beginning of a string.
+- `trim()`			:	Removes the characters from both ends of the string.
+- `trimEnd()`		:	Removes the characters from the end of the string.
+- `trimStart()`		:	Removes characters from the beginning of a string.
 - `toWellFormed()`	:	Converts a string to a well-formed string.
 
 ### Non-standard methods
 
-- `chop()`			:	Removes specified characters from the start and end of a string.
-- `chopEnd()`		:	Removes trailing characters from the string until the last occurrence of the specified character.
-- `chopStart()`		:	Removes leading characters from the string until the first occurrence of the specified character.
 - `contains()`		:	Checks if the string contains a specific substring.
 - `format()`		:	Formats a string by replacing placeholders with values.
 - `indexAfter()`	:	Returns the index of the character immediately following the first occurrence of the specified substring.
--- String/String-nonstandard.js
+++ String/String-nonstandard.js
@@ -5,9 +5,6 @@
  *  @copyright: MIT
  * 
  *	Non-standard functions:
- *		chop()			:	Removes specified characters from the start and end of a string.
- *		chopEnd()		:	Removes trailing characters from the string until the last occurrence of the specified character.
- *		chopStart()		:	Removes leading characters from the string until the first occurrence of the specified character.
  *		contains()		:	Checks if the string contains a specific substring.
  *		format()		:	Formats a string by replacing placeholders with values.
  *		indexAfter()	:	Returns the index of the character immediately following the first occurrence of the specified substring.
@@ -15,69 +12,6 @@
  *		isEmpty()		:	Checks if a string is empty.
  *		reverse()		:	Reverses the order of the characters in the string
  */
-
-/**
- * Removes specified characters from the start and end of a string.
- *
- * @param {string} chr - The character(s) to be removed.
- * @return {string} - The modified string with the specified characters removed.
- * @example
- *     > $.writeln("   hello world   ".chop());
- *     "hello world"
- * @example
- * > $.writeln("*-*-*-hello world*-*-*-".chop("*-"));
- *     "hello world"
- */
-if (!String.prototype.chop) {
-    String.prototype.chop = function (chr) {
-        return chr ? this.chopStart(chr).chopEnd(chr) : this.replace(/^\s+|\s+$/g, "");
-    };
-}
-
-/**
- * Removes trailing characters from the end of a string.
- *
- * @param {string} chr - The character or characters to remove.
- * @return {string} - The modified string.
- * @example 
- *	> $.writeln("   hello world   ".chopEnd());
- *     "   hello world"
- * @example 
-    > $.writeln("*-*-*-hello world*-*-*-".chopEnd("*-"));
- *     "*-*-*-hello world"
- */
-if (!String.prototype.chopEnd) {
-    String.prototype.chopEnd = function (chr) {
-        if (!chr) {
-            return this.replace(/\s+$/, "");
-        } else {
-            return this.endsWith(chr)
-                ? this.slice(0, -chr.length).chopEnd(chr)
-                : this;
-        }
-    };
-}
-
-
-/**
- * Removes leading characters from the string until the first occurrence of the specified character.
- *
- * @param {string} chr - The character to search for.
- * @return {string} - The resulting string after removing the leading characters.
- * @example > $.writeln("   hello world   ".chopStart());
- *     "hello world   "
- * @example > $.writeln("*-*-*-hello world*-*-*-".chopStart("*-"));
- *     "hello world*-*-*-"
- */
-if (!String.prototype.chopStart) {
-    String.prototype.chopStart = function (chr) {
-        if (!chr) {
-            return this.replace(/^\s+/, "");
-        } else {
-            return this.startsWith(chr) ? this.substring(chr.length).chopStart(chr) : this;
-        }
-    };
-}
 
 /**
  * Checks if the string contains a specific substring.
