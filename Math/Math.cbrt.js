2fedc8f49b7c55899454166bc68b4e1eea8f7c85
serfegyed <serfegyed@users.noreply.github.com>
2024-02-29 17:49:19 +0100

    new: Some missing Math functions

---
+++ Math/Math.cbrt.js
@@ -0,0 +1,10 @@
+﻿Math.cbrt = function(x) {
+    if (x === 0) return 0;
+    var negate = x < 0,
+        result;
+    if (negate) {
+        x = -x;
+    }
+    result = Math.exp(Math.log(x) / 3);
+    return negate ? -result : result;
+};
---
+++ Math/Math.log10.js
@@ -0,0 +1,5 @@
+if (!Math.log10) {
+    Math.log10 = function(x) {
+        return Math.log(x) / Math.LN10;
+    };
+}
